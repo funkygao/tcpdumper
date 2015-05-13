0409b385aa5f6289debecef52867ed9c83f5a5cf
funkygao <funky.gao@gmail.com>
2015-05-13 11:46:50 +0800

    color of report

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 tcpdump output analyzer
 
 tcpdumper helps to analyze tcp connection timeout, connection reset, etc.
+
+Note: MUST be root to run this tcpdumper.
+
+### Report
+
+- fist FIN will be red in color
+- blink if SYN retransmit occurs
+
+![sample](https://github.com/funkygao/tcpdumper/blob/master/.resources/sample.png)
--- report/report.go
+++ report/report.go
@@ -6,24 +6,80 @@ import (
 	"os"
 	"strings"
 	"time"
+
+	"github.com/funkygao/golib/color"
 )
 
 var errBadLine = errors.New("bad tcpdump output line")
 
+const (
+	SYNC_SEND = ">S"
+	FIN       = "F"
+)
+
+type trip struct {
+	src, dst, flag string
+}
+
+type report map[string][]trip
+
 func ShowReportAndExit(startedAt time.Time, lines []string) {
 	fmt.Printf("%d lines, elapsed: %s\n", len(lines), time.Since(startedAt))
 
+	var rp = make(report, 1<<10)
+
 	for _, line := range lines {
-		fmt.Println(line)
 		src, dst, flag, err := lineInfo(line)
 		if err != nil {
 			continue
 		}
-		fmt.Println(src, dst, flag)
+
+		if _, present := rp[src]; present {
+			rp[src] = append(rp[src], trip{src, dst, ">" + flag})
+		} else {
+			rp[src] = []trip{trip{src, dst, ">" + flag}}
+		}
+
+		if _, present := rp[dst]; present {
+			rp[dst] = append(rp[dst], trip{src, dst, "<" + flag})
+		} else {
+			rp[dst] = []trip{trip{src, dst, "<" + flag}}
+		}
+
+	}
+
+	for endpoint, trips := range rp {
+		fmt.Printf("%21s", endpoint)
+		if len(trips) > 1000 {
+			fmt.Printf(" skipped\n")
+			continue
+		}
+
+		syncSentN := 0
+		finN := 0
+		for _, t := range trips {
+			if t.flag == SYNC_SEND {
+				syncSentN++
+				if syncSentN > 1 {
+					// retransmit
+					t.flag = color.Colorize([]string{color.FgBlue, color.Blink},
+						t.flag)
+				} else {
+					t.flag = color.Blue(t.flag)
+				}
+			} else if strings.Contains(t.flag, FIN) {
+				finN++
+				if finN == 1 {
+					t.flag = color.Red(t.flag)
+				}
+
+			}
+			fmt.Printf(" %-2s", t.flag)
+		}
+		fmt.Println()
 	}
 
 	os.Exit(0)
-
 }
 
 func lineInfo(line string) (src, dst, flag string, err error) {
